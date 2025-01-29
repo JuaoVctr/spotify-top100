872c10de06e78062563044dccec1bf13b9b24115
JuaoVctr <joaovictor_freire2@hotmail.com>
2025-01-29 16:32:14 -0300

    main.py

--- main.py
+++ main.py
@@ -38,4 +38,4 @@ def callback():
     return f"Playlist criada com sucesso! <a href='{playlist['external_urls']['spotify']}'>Acesse aqui</a>."
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
